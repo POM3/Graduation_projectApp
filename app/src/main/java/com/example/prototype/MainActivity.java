e01f9abda22ce7ae56bb22e62cfbe00e489e1c2e
POM3 <59609080+POM3@users.noreply.github.com>
2022-02-06 20:37:48 +0900

    2021-02-06,수정 UI

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.prototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button setting_voice,sunglass_connect,googlemap_connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setting_voice=(Button) findViewById(R.id.setting_voice);
+        sunglass_connect=(Button) findViewById(R.id.sunglass_connect);
+        googlemap_connect=(Button) findViewById(R.id.googlemap_connect);
+
+
+        setting_voice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, voiceOption.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
